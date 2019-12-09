50f50e0cd382a7dbb7470fff9c418264465f8a01
jxgong <jasgong@gmail.com>
2019-12-09 10:34:50 -0500

    added auxiliary files

---
+++ ppm.h
@@ -0,0 +1,10 @@
+#ifndef __PPM_H__
+#define __PPM_H__
+
+/* from 418 HW2 */
+
+struct Image;
+
+void writePPMImage(const Image* image, const char *filename);
+
+#endif
---
+++ util.h
@@ -0,0 +1,10 @@
+
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <algorithm>
+/* from 418 HW2 */
+
+#define CLAMP(x, minimum, maximum) std::max(minimum, std::min(x, maximum))
+
+#endif
